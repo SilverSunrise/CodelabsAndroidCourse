e8c09cfe36d9bf38bf0711ac968ef0c9e03fd93f
SilverSunrise <39579361+SilverSunrise@users.noreply.github.com>
2019-12-24 23:24:28 +0200

    add new gray button "Zero" 1. When called change the show_count to 0. 2. When show_count >= 1, background is pink.

--- app/src/main/java/com/example/android/com/MainActivity.java
+++ app/src/main/java/com/example/android/com/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.android.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,12 +14,14 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    private Button cShowBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = findViewById(R.id.show_count);
+        cShowBtn = findViewById(R.id.button_zero);
     }
 
     public void ShowToast(View view) {
@@ -25,13 +29,21 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
     }
 
+
     @SuppressLint("SetTextI18n")
     public void countUp(View view) {
+
         mCount++;
         if(mShowCount != null) {
             mShowCount.setText(Integer.toString(mCount));
+            cShowBtn.setBackgroundColor(Color.rgb(231, 84, 128));
         }
     }
 
 
+    public void ShowZero(View view) {
+        mShowCount.setText("0");
+        mCount = 0;
+        cShowBtn.setBackgroundColor(Color.rgb(133, 133, 133));
+    }
 }
