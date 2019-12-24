8d2ef21c9016da0ab07d6a52b2fdc19c5e0ce31c
SilverSunrise <39579361+SilverSunrise@users.noreply.github.com>
2019-12-24 20:37:44 +0200

    Created UI(Toast + Count)

--- app/src/main/java/com/example/android/com/MainActivity.java
+++ app/src/main/java/com/example/android/com/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.android.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = findViewById(R.id.show_count);
     }
+
+    public void ShowToast(View view) {
+        Toast toast = Toast.makeText(this,"Hello Toast", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void countUp(View view) {
+        mCount++;
+        if(mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+
+
 }
