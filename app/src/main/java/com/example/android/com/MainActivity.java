622393dab02c58839c153883fc81d21d669798c1
SilverSunrise <39579361+SilverSunrise@users.noreply.github.com>
2019-12-25 03:09:32 +0200

    Changed the Toast button to SayHello + passed value show_count in second activity. So that it launches a new Activity to display the word "Hello!" and the current count.

--- app/src/main/java/com/example/android/com/MainActivity.java
+++ app/src/main/java/com/example/android/com/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -20,9 +21,10 @@ public class MainActivity extends AppCompatActivity {
         mShowCount = findViewById(R.id.show_count);
     }
 
-    public void ShowToast(View view) {
-        Toast toast = Toast.makeText(this,"Hello Toast", Toast.LENGTH_SHORT);
-        toast.show();
+    public void showToast(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("count", mShowCount.getText().toString());
+        startActivity(intent);
     }
 
     @SuppressLint("SetTextI18n")
--- app/src/main/java/com/example/android/com/SecondActivity.java
+++ app/src/main/java/com/example/android/com/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.android.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        String count = getIntent().getStringExtra("count");
+        TextView textView = findViewById(R.id.showCountSecond);
+        textView.setText(count);
+
+
     }
 }
