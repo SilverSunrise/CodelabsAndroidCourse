36e6e9577252ac91b9a06cd1b63df3ff29af3551
SilverSunrise <39579361+SilverSunrise@users.noreply.github.com>
2020-01-14 17:05:24 +0200

    changed logs `MainActivity` to TAG, add textCongrats in string.xml, add padding in dimens, renamed method

--- app/src/main/java/com/example/android/com/MainActivity.java
+++ app/src/main/java/com/example/android/com/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.android.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,25 +10,28 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "myLogs";
 
-    private EditText EditName;
+    private EditText editTextName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MainActivity", "Happy birthday to Mari");
-        Log.e("MainActivity", "Happy birthday to Mari");
-        Log.i("MainActivity", "Happy birthday to Mari");
-        Log.w("MainActivity", "Happy birthday to Mari");
-        EditName = findViewById(R.id.edit_name);
+        Log.d(TAG, "найдём View-элемент");
+        editTextName = findViewById(R.id.edit_name);
+
+        Log.e(TAG, "Happy birthday to Mari");
+        Log.i(TAG, "Happy birthday to Mari");
+        Log.w(TAG, "Happy birthday to Mari");
     }
 
 
-    @SuppressLint("SetTextI18n")
-    public void show_congratulation(View view) {
-        TextView show = findViewById(R.id.show_congratulation);
-        String name = EditName.getText().toString();
-        show.setText("Happy birthday to " + name);
+    public void showCongratulation(View view) {
+        Log.d(TAG, "находим элемент по его ID");
+        TextView showCongrats = findViewById(R.id.show_congratulation);
+        String name = editTextName.getText().toString();
+        Log.d(TAG, "меняем текст в TextView (show)");
+        showCongrats.setText(String.format("%s%s", getString(R.string.textCongrats), name));
     }
 }
