e912f8a73fc791a6aacceb7cd3dfc3e47ee1a581
SilverSunrise <39579361+SilverSunrise@users.noreply.github.com>
2020-01-16 17:22:09 +0200

    finished Two Activities

--- app/src/main/java/com/example/android/com/MainActivity.java
+++ app/src/main/java/com/example/android/com/MainActivity.java
@@ -1,25 +1,52 @@
 package com.example.android.com;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.android.com.activity_second.MESSAGE";
+    public static final int TEXT_REQUEST = 1;
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private EditText mMessageEditText;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMessageEditText = findViewById(R.id.inputMessage);
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
     }
 
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG,getString(R.string.logD_mainActivity));
         Intent intent = new Intent(this, activity_second.class);
-        startActivity(intent);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode,
+                                 int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(activity_second.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
