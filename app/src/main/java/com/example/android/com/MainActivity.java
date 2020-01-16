34d65267f87157f279da6a1fb29ffca1c13ce7d0
SilverSunrise <39579361+SilverSunrise@users.noreply.github.com>
2020-01-16 16:07:12 +0200

    Created both activities + made transition between them

--- app/src/main/java/com/example/android/com/MainActivity.java
+++ app/src/main/java/com/example/android/com/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.android.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG,getString(R.string.logD_mainActivity));
+        Intent intent = new Intent(this, activity_second.class);
+        startActivity(intent);
     }
 }
