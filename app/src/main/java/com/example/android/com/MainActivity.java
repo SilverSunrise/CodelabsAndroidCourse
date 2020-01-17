c5906a4101ade2306fc47fd8f2316bb8cc7dbe03
SilverSunrise <39579361+SilverSunrise@users.noreply.github.com>
2020-01-17 17:17:35 +0200

    added method "openWebsite"

--- app/src/main/java/com/example/android/com/MainActivity.java
+++ app/src/main/java/com/example/android/com/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.android.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mWebsiteEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openWebsite(View view) {
+        mWebsiteEditText = findViewById(R.id.website_editText);
+        String url = mWebsiteEditText.getText().toString();
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void openLocation(View view) {
+    }
+
+    public void shareText(View view) {
     }
 }
