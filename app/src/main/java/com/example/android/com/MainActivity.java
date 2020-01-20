eed26d81af5d7ae4ee450432d7e7c97bfbcb8725
SilverSunrise <39579361+SilverSunrise@users.noreply.github.com>
2020-01-20 11:30:03 +0200

    added other methods + openPhoto (homework)

--- app/src/main/java/com/example/android/com/MainActivity.java
+++ app/src/main/java/com/example/android/com/MainActivity.java
@@ -1,18 +1,21 @@
 package com.example.android.com;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
-import java.net.URL;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mWebsiteEditText;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final String TAG = "ImplicitIntent";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +24,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openWebsite(View view) {
-        mWebsiteEditText = findViewById(R.id.website_editText);
+        EditText mWebsiteEditText = findViewById(R.id.website_editText);
         String url = mWebsiteEditText.getText().toString();
         Uri webpage = Uri.parse(url);
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
+        } else {
+            Log.d(TAG, getString(R.string.logD_mainActivity_message));
         }
     }
 
     public void openLocation(View view) {
+        EditText mLocationEditText = findViewById(R.id.location_editText);
+        String loc = mLocationEditText.getText().toString();
+        Uri adressUri = Uri.parse(getString(R.string.geoAdress_mainActvity_location) + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, adressUri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d(TAG, getString(R.string.logD_mainActivity_message));
+        }
     }
 
     public void shareText(View view) {
+        EditText mShareEditText = findViewById(R.id.share_editText);
+        String txt = mShareEditText.getText().toString();
+        String mimeType = getString(R.string.shareText_mainActvity_mimeType);
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle(getString(R.string.shareText_mainActivity_ChooserTitle))
+                .setText(txt)
+                .startChooser();
+    }
+
+    public void openPhoto(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } else {
+            Log.d(TAG, getString(R.string.logD_mainActivity_message));
+        }
     }
 }
