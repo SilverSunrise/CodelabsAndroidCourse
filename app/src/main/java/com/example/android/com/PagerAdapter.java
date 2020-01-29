5a25683eb025c3f1a368404342ad6482fd0b6c8a
SilverSunrise <39579361+SilverSunrise@users.noreply.github.com>
2020-01-29 12:48:36 +0200

    created 3 tabs (course)

---
+++ app/src/main/java/com/example/android/com/PagerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.android.com;
+
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+
+    PagerAdapter(FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new TabFragment1();
+            case 1:
+                return new TabFragment2();
+            case 2:
+                return new TabFragment3();
+            default:
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
