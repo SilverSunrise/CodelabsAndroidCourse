d16f40d519843df777aaea805d3883cd589d1b7e
SilverSunrise <39579361+SilverSunrise@users.noreply.github.com>
2020-01-30 16:37:37 +0200

    Input Controls tasks 1,2

--- app/src/main/java/com/example/android/com/OrderActivity.java
+++ app/src/main/java/com/example/android/com/OrderActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -17,4 +20,26 @@ public class OrderActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.tv_activityOrder_order);
         textView.setText(message);
     }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()) {
+            case R.id.sameDay:
+                if (checked) displayToast(getString(R.string.rb_activityOrder_textSameDay));
+                break;
+            case R.id.nextDay:
+                if (checked) displayToast(getString(R.string.rb_activityOrder_textNextDay));
+                break;
+            case R.id.pickUp:
+                if (checked) displayToast(getString(R.string.rb_activityOrder_textPickUp));
+                break;
+            default:
+                break;
+        }
+    }
+
 }
