906df169813ce147b26c7c7d17f09cb7aa06b242
SilverSunrise <39579361+SilverSunrise@users.noreply.github.com>
2020-01-31 16:27:16 +0200

    Menus and pickers challenge 2

--- app/src/main/java/com/example/android/com/MainActivity.java
+++ app/src/main/java/com/example/android/com/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.android.com;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.btn_time);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), getString(R.string.timepicker));
+            }
+        });
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        displayToast(getString(R.string.hour) + hourOfDay + getString(R.string.minute) + minute);
     }
 }
